37ba7bc06dc4ad2f7b41289d2b4ab7e43197f814
Bruno Monteiro <brinuz@gmail.com>
2020-01-29 17:01:47 +0000

    Fix linting issues

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
     coverageDirectory: "coverage",
     moduleFileExtensions: [
-        'js',
-        'jsx',
-        'json',
+        "js",
+        "jsx",
+        "json",
     ],
-    setupFilesAfterEnv: ['<rootDir>/setup_tests.js'],
-    transformIgnorePatterns: ['<rootDir>/node_modules/'],
+    setupFilesAfterEnv: ["<rootDir>/setup_tests.js"],
+    transformIgnorePatterns: ["<rootDir>/node_modules/"],
 };
